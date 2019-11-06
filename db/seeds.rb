34ec4dc38aae21600642a98e3798017ce27a7697
Ilham Rahman <ilhaamrahman@gmail.com>
2019-11-06 17:32:12 +0900

    not finished

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,15 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+require 'faker'
+
+puts "Creating flats...."
+
+5.times do
+  Flat.create!(
+    name: "Apt. #{Faker::Address.street_name}",
+    address: Faker::Address.street_address,
+    description: Faker::Restaurant.description,
+    price_per_night: (50..150).to_a.sample,
+    number_of_guests: (1..3).to_a.sample
+  )
+end
+
+puts "...finished"
